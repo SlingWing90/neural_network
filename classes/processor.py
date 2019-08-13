6bae9440d904293b9c84258be09b0fb22ae09a8e
brandon.hoeltgen <brandon.hoeltgen@web.de>
2019-08-13 11:09:39 +0200

    Multiple Y-Values in Output of process

--- classes/processor.py
+++ classes/processor.py
@@ -41,8 +41,10 @@ class Processor:
 					error_sum = error_sum + (error*error)
 					
 					row.append(error)
-
-					self.network[len(self.network)-1].update_weight(self.learning_rate, error);
+					# - y_t
+					#print("Y_T: "+str(y_t))
+					#self.network[len(self.network)-1].update_weight(self.learning_rate, error); original
+					self.network[len(self.network)-(y_t+1)].update_weight(self.learning_rate, error)
 
 					self.table_output.append(row)
 			
@@ -68,11 +70,21 @@ class Processor:
 		for v in range(0, len(x_input)):
 			self.network[v].input = x_input[v] 
 			
+		#print(str(len(self.y_table[0])))
 		y = 0
 		for v in range(len(x_input), len(self.network)):
+			#print(str(v))
 			y = self.network[v].calc_y();
 		
-		return round(y, 0)
+		#
+		y_val = []
+		for v in range(len(self.network) - len(self.y_table[0]), len(self.network)):
+			#print(str(v))
+			y = self.network[v].calc_y();
+			y_val.append(y)
+		
+		#print(y_val)
+		return y_val #round(y, 0)
 
 	def print_weight(self, children):
 		if not children.prev_neuron is None:
--- neural_network.py
+++ neural_network.py
@@ -19,6 +19,8 @@ neuron_3 = Neuron("3", 0.8, [[0.5, neuron_1], [0.4, neuron_2]])
 neuron_4 = Neuron("4", -0.1, [[0.9, neuron_1], [1.0, neuron_2]])
 neuron_5 = Neuron("5", 0.3, [[-1.2, neuron_3], [1.1, neuron_4]])
 
+#neuron_6 = Neuron("6", 0.5, [[-0.2, neuron_3], [0.2, neuron_4]])
+
 neuronal_network = [];
 neuronal_network.append(neuron_1)
 neuronal_network.append(neuron_2)
@@ -26,9 +28,12 @@ neuronal_network.append(neuron_3)
 neuronal_network.append(neuron_4)
 neuronal_network.append(neuron_5)
 
+#neuronal_network.append(neuron_6)
+
 X = [[0, 0], [1, 0], [0, 1], [1, 1]]
 Y = [[0], [1], [1], [0]]
-#Y = [[0, 0], [0, 1], [1, 0], [1, 1]]
+#Y = [[0, 0], [1, 1], [1, 1], [0, 0]]
+#Y = [[0, 0], [1, 0], [0, 1], [0, 0]]
 
 #learning_rate = 0.1
 #learning_threshold = 0.001
@@ -38,6 +43,14 @@ pr = Processor(neuronal_network, X, Y)
 print("Weights:")
 pr.print_weight(neuron_5)
 
+#print("Weights:")
+#pr.print_weight(neuron_6)
+
+# TEST
+#val = pr.process([1, 1])
+#print(val)
+
+
 print("")
 print("Example: ")
 for n in X:
@@ -45,7 +58,8 @@ for n in X:
 	x2 = n[1]
 
 	val = pr.process([x1, x2])
-	print(str(x1)+" "+str(x2)+" "+str(val))
+	print(str(x1)+" "+str(x2))
+	print(val)
 print("")
 
 pr.learn(1)
@@ -58,6 +72,10 @@ print("")
 print("Weights:")
 pr.print_weight(neuron_5)
 
+#print("")
+#print("Weights 2:")
+#pr.print_weight(neuron_6)
+
 print("")
 print("Example: ")
 for n in X:
@@ -65,4 +83,5 @@ for n in X:
 	x2 = n[1]
 
 	val = pr.process([x1, x2])
-	print(str(x1)+" "+str(x2)+" "+str(val))
+	print(str(x1)+" "+str(x2))
+	print(val)
